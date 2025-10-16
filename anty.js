671194804ca18ed7f6567ff90238652a5904e747
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-16 20:50:05 +0600

    added some code

---
+++ anty.js
@@ -0,0 +1,12 @@
+/**
+ *  comparison 
+ * 1. bigger than (>)
+ * 2. smaller than (<)
+ * 3. bigger than or equal to (>=)
+ * 4. smaller than or equal to (<=)
+ * 5. equal to (==)
+ * 6. not equal to (!=) 
+ *  ----------------------------------
+ * and (&&)
+ * or (||)
+ */
---
+++ variable.js
@@ -0,0 +1,29 @@
+// variable ==> container 
+
+
+// number
+var sunglasses = 80;
+
+// string
+var subject = "JavaScript";
+
+// boolean
+var apple  = true;
+
+
+
+// name convention
+// 1.single word
+// 2.no quotes
+// 3.no space
+// 4.no keyword
+// 5.cannot start with number
+// 6.case sensitive
+// 7.use camelCase
+
+/**
+ * operators
+ * 1.arithmetic operators (+, -, *, /, %)
+ * 2.assignment operators (=, +=, -=, *=, /=, %=)
+ * 
+ */
