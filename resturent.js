d68d8d3169900e7af9034109980722e77cbc4bf3
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-17 20:38:41 +0600

    added code

---
+++ resturent.js
@@ -0,0 +1,12 @@
+const age = 17;
+const foodPrice = 500;
+
+
+if (age > 12) {
+    console.log('they are free no pay ')
+}
+else if ((age > 12 && age < 18)|| age > 50) {
+    // 50% discount
+    const discount = foodPrice * 50 /
+        console.log(foodPrice - discount)
+}
