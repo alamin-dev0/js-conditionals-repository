4c08fe862e97403c7d7f1dfe670ecabad10981e8
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-17 14:51:13 +0600

    added some code

---
+++ if.js
@@ -0,0 +1,58 @@
+/**
+ * tui jodi amake khaos tahole ami asbo 
+ * 
+ * --------------------------------
+ * if you buy food for me i will come
+ * -------------------------------- 
+ * if biryani price less then 300 taka then i will buy it
+ * --------------------------------
+ *  -------------------STRUCTURE --------------------
+ * if (condition) {
+ *      // code to be executed if condition is true
+ *  }
+ * --------------------------------
+ * if (condition) {
+ *      // code to be executed if condition is true
+ *  } else {
+ *      // code to be executed if condition is false
+ *  }
+ * 
+ */
+
+// if (30 > 20) {
+//     console.log("30 is greater than 20");
+// }
+// if (30 < 20) {
+//     console.log("30 is less than 20");
+// }
+// else {
+//     console.log("30 is not less than 20");
+// }
+
+
+var biryaniPrice = 250;
+if (biryaniPrice < 300) {
+    console.log("I will buy biryani");
+}
+else {
+    console.log("I will eat at home");
+}
+
+
+
+/**
+ * if (condition1) {
+ *      // code to be executed if condition1 is true
+ *  } 
+ * else {
+ *      execute code if the condition if false 
+ * }
+ */
+
+var weight = 20;
+if (weight <= 20){
+    console.log("i will carry it by myself");
+}
+else {
+    console.log('i will rent a rickshaw');
+}
