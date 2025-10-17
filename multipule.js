14a8e93023ed2a5f51fc6941fb00909cfef8c705
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-17 17:43:27 +0600

    added some code

---
+++ multipule.js
@@ -0,0 +1,51 @@
+const salary = 40001;
+const isBCS = true;
+const height = 71;
+const hasCar = true;
+
+// if (salary > 40000 && height > 66) {
+//     console.log('he is better')
+// }
+// else {
+//     console.log('he is good')
+// }
+
+// if (salary > 60000 || height > 80) {
+//     console.log('He is better')
+
+// }
+
+// else {
+//     console.log('Go now')
+// }
+
+
+// more and more condition 
+
+// if (salary > 40000 && height > 72 && isBCS == true) {
+//     console.log('He is better')
+
+// }
+
+// else {
+//     console.log('Go now')
+// }
+
+
+
+// ----------------- COMPLEX CONDITION ----------------------
+// if ((salary > 40000 && hasCar == true) || isBCS == true) {
+//     console.log('All agreed')
+// }
+
+// else {
+//     console.log('Not agreed')
+
+// }
+    if ((salary > 400002 || hasCar == true) && isBCS == true) {
+    console.log('All agreed')
+}
+
+else {
+    console.log('Not agreed')
+}
