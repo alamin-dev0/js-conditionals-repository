5991f247e3a31aede665e7a9a92b700cd117816b
alamin-dev0 <alamin.dev01@gmail.com>
2025-10-21 19:20:37 +0600

    added code

--- task.js
+++ task.js
@@ -10,6 +10,6 @@
  * 4. ei khane amra ekta kaj korbo amar mark jodi 80 hoy tahole ami amar fd er result check dibo tar por tar ta check diye tar result jodi 80 ba tar beshi hoy tahole lunch korte jabo ar jodi na hoy 60/70 hoy thaole good luck bole dibo ar jodi tao na hoy jodi 40/50  hoy tahole sms unseen kore rakhbo ar jodi 40 er kom hoy tahole take block diye rakhbo  ***** abar ami nijei jodi 80 nice pay tahole sad hobo ...
  * 5.ei khane number one ar number two ar amake ekta create korte hobe result name diye ar number one jodi number 2 er theke boro hoy thaole reslt hobe number one er 2gun ar jodi number one jodi number 2 er theke boro na hoy tahole 2 ta ke plus kore dite hobe
  * write a simple if- else abar ternary oprator diyeo korte hobe 
- * 
+ * 6. ticket fare calculator ei khane jodi children 10 bochor er nice hole free hobe ar jodi student hoy tahole 50% discount hobe ar senior sitizens hoy tahole 15% discount hobe otherwise regular ticket fare 800 tk 
  * 
  */
